6e78076ae28b97cc9dd7aaf82b38ed840089dae2
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-10 18:00:48 +0300

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,32 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks() // =>42
 
-  return <h1>Colorado State Parks!</h1>;
+  console.log(trees);
+// => "Aspen and Pine"
+
+wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+console.log(RMFunctions.trees);
+// => "Aspen and Pine"
+
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+// RMFunctions.elevation();
+// // => Attempted import error
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </div>
+  );
 }
+
+
+export default ColoradoStateParks;
